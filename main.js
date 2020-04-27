51482bed1249e35e7f486dfd64d90f205c11f783
Anastasiia Shadrinova <nastya.shadrinova@yandex.ru>
2020-04-27 21:41:46 +0900

    Домашнее задание 2

--- main.js
+++ main.js
@@ -1,40 +1,92 @@
 "use strict";
 
-const products = [
-    {id: 1, title: "Notebook", price: 20000},
-    {id: 2, title: "Mouse", price: 1500},
-    {id: 3, title: "Keyboard", price: 5000},
-    {id: 4, title: "Gamepad", price: 4500},
-];
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+        this._render();
+        this._countTotalSum();
+    }
 
-/* const renderProduct = (title, price, color = "white", img = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRVZwwnmI8ZtMX6gOgMH3N5pu1P84DNznv9ZD6zPDZwGJ7V3Fee&usqp=CAU") => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <p>${color}</p>
-                <img src=${img} alt="img">
-                <button class="by-btn">Добавить в корзину</button>
-            </div>`;
-}; */
+    _fetchProducts() {
+        this.goods = [
+            {id: 1, title: "Notebook", price: 20000},
+            {id: 2, title: "Mouse", price: 1500},
+            {id: 3, title: "Keyboard", price: 5000},
+            {id: 4, title: "Gamepad", price: 4500},
+        ];
+    }
 
-// стрелочная функция в сокращенном написании
-const renderProduct = (title, price, color = "white", img = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRVZwwnmI8ZtMX6gOgMH3N5pu1P84DNznv9ZD6zPDZwGJ7V3Fee&usqp=CAU") => `<div class="product-item"> <h3>${title}</h3> <p>${price}</p> <p>${color}</p> <img src=${img} alt="img"> <button class="by-btn">Добавить в корзину</button> </div>`;
+    _render() {
+        const block = document.querySelector(this.container);
 
-/* const renderProducts = (list) => {
-    const productList = list.map((good) => {
-        return renderProduct(good.title, good.price);
-    });
-    document.querySelector(".products").innerHTML = productList;
-} */
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
 
-// чтобы продукты отображались без запятых
-const renderProducts = (list) => {
-    list.forEach(function(good) {
-        document.querySelector(".products").insertAdjacentHTML('beforeend', renderProduct(good.title, good.price));
-    });
+    _countTotalSum() {
+        const block = document.querySelector('.totalSum');
+        let sum = 0;
+        for (let product of this.allProducts) {
+            sum += product.price;
+        }
+        let sumElement = `Суммарная стоимость всех товаров: <span>${sum}</span> \u20bd`;
+        block.insertAdjacentHTML('beforeend', sumElement);
+    }
 }
 
-renderProducts(products);
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
 
-// Запятая выводится из-за того, что productList - это массив, элементы в нем отделяются ","
-  
+    render() {
+        return `<div class="product-item data-id="${this.id}">
+                    <img src="${this.img}" alt="Some img">
+                    <div class="desc">
+                        <h3>${this.title}</h3>
+                        <p>${this.price} \u20bd</p>
+                        <button class="buy-btn">Купить</button>
+                    </div>
+                </div>`;
+    }
+}
+
+const productList = new ProductList()
+
+
+/* class Cart {
+    constructor() {
+        this.addedProducts = [];
+        this._render();
+    }
+
+    _render() {
+        const block = document.querySelector('.cart');
+        const cartElement = new CartElement();
+        this.addedProducts.push(cartElement);
+        block.insertAdjacentHTML('beforeend', cartElement.render());
+    }
+    Этот метод срабатывает при нажатии на кнопку для добавления товара
+}
+
+class CartElement extends ProductItem {
+    constructor(product, container = '.added') {
+        super(product);
+        this.container = container;
+    }
+
+    remove() {
+        document.querySelector(this.container).remove();
+    }
+}
+
+new Cart(); */
